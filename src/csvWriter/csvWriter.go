1b04d91f435f9a371a071306390ba923c320ac61
Keegan <keeganpatton@gmail.com>
2024-12-23 20:58:24 -0600

    Threadsafe concurrent CSV writer

--- src/csvWriter/csvWriter.go
+++ src/csvWriter/csvWriter.go
@@ -1,19 +1,42 @@
 package csvWriter
 
 import (
-	"os"
-	"log"
-	"fmt"
 	"context"
 	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
 	"path/filepath"
-	"google.golang.org/api/iterator"
+	"sync"
+
 	"cloud.google.com/go/bigquery"
+	"google.golang.org/api/iterator"
 )
 
 
 
+// Progress Bar callback
 type ProgressCallBack func(float64)
+
+
+// Thread Safe CSV writer stuff
+type SafeWriter struct {
+	mutex sync.Mutex
+	writer *csv.Writer
+}
+
+func (s *SafeWriter) Write(record []string) error {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	return s.writer.Write(record)
+}
+
+func (s *SafeWriter) Flush() {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	s.writer.Flush()
+}
+
 
 
 /* Accepts a *bigquery.Job struct as an argument and writes to a csv file. */
@@ -46,7 +69,8 @@ func WriteCsv(job *bigquery.Job, progressCb ProgressCallBack) error {
 	
 
 	writer := csv.NewWriter(file)
-	defer writer.Flush()
+	writeSafe := &SafeWriter{writer: writer}
+	defer writeSafe.Flush()
 		
 
 	header := make([]string, 0)
@@ -55,11 +79,37 @@ func WriteCsv(job *bigquery.Job, progressCb ProgressCallBack) error {
 	}
 	
 	
-	err = writer.Write(header)
+	err = writeSafe.Write(header)
 	if err != nil {
 		log.Fatal("Error writing header:", err)
 	}
 	
+
+
+	// GoRoutine stuff
+	rowCount := it.TotalRows	
+	rowsChan := make(chan []bigquery.Value)
+	errChan := make(chan error)
+	var wg sync.WaitGroup
+
+	workers := 8
+	for i := 0; i < workers; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for row := range rowsChan {
+				csvRow := make([]string, len(row))
+				for i, value := range row {
+					csvRow[i] = fmt.Sprintf("%v", value)
+				}
+				if err := writeSafe.Write(csvRow); err != nil {
+					errChan <- fmt.Errorf("error writing to csv: %v", err)
+					return
+				}
+			}
+		}()
+	}
+
 
 
 	// Main Iterator
@@ -74,26 +124,24 @@ func WriteCsv(job *bigquery.Job, progressCb ProgressCallBack) error {
 			return fmt.Errorf("iterator.Next: %v", err)
 		}
 
-
-		csvRow := make([]string, len(row))
-		for i, value := range row {
-			csvRow[i] = fmt.Sprintf("%v", value)
-		}
-		if err := writer.Write(csvRow); err != nil {
-			return fmt.Errorf("error writing to csv: %v", err)
-		}
-
-
-		rowCount := it.TotalRows	
+		// Send curr row to the curr channel
+		rowsChan <- row
 		rowsWritten++
-		
 		if rowCount > 0 {
 			progress := float64(rowsWritten) / float64(rowCount)
 			progressCb(progress)
 		}
+	}
+
+	close(rowsChan)
+	wg.Wait()
+
+	select {
+	case err := <- errChan:
+		return err
+	default:
+	}
 		
-	}	
-	
 	log.Println("Success")
 	return nil
 
